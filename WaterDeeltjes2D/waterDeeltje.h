a8d25fee4fe4bf1b9cb2b40798b7a91771dbcb59
Sam Slo <samman350@hotmail.com>
2025-11-27 05:50:07 +0100

    Add alternative string topology

--- WaterDeeltjes2D/waterDeeltje.h
+++ WaterDeeltjes2D/waterDeeltje.h
@@ -4,8 +4,7 @@
 #include <iostream>
 #include "math2D.h"
 #include <vector>
-
-constexpr float M_PI = (float)3.14159265358979323846;
+#include <random>
 
 class waterDeeltje {
 public:
@@ -18,14 +17,15 @@ public:
     std::vector<waterDeeltje*> neighbours = std::vector<waterDeeltje*>();
     std::vector<float> offsets = std::vector<float>();
     float mass;
-    bool simulable; // is the particle motion dictated by forces (true), or static/other (false)?
+    bool simulable = true; // is the particle motion dictated by forces (true), or static/other (false)?
+    bool fixed; // fixed particle?
 
-    waterDeeltje(Vec2 initPos, Vec2 initVel, Vec2 initAcc, float massParticle, bool simulatable) :
+    waterDeeltje(Vec2 initPos, Vec2 initVel, Vec2 initAcc, float massParticle, bool fixd) :
         position(initPos), // initializer list
         velocity(initVel),
         newAcceleration(initAcc),
         mass(massParticle),
-        simulable(simulatable)
+        fixed(fixd)
     {}
 };
 
@@ -35,13 +35,13 @@ public:
 
     ParticleSystem(const int N_Particles, const float r_cutoff) {
 
-        for (int i = 0; i < N_Particles; i++) {
-            if (i == 0 || i == N_Particles - 1) {
+        for (int i = 0; i < 11; i++) { // initialize the simulation
+            if (i == 10) {
                 WaterDeeltjes.emplace_back(Vec2((float)i, 0.0f),
                     Vec2(0.0f, 0.0f),
                     Vec2(0.0f, 0.0f),
                     1.0f,
-                    false); // fix outer particles
+                    true); // fix outer particles in grid
 
             }
             else {
@@ -49,7 +49,41 @@ public:
                     Vec2(0.0f, 1.0f),
                     Vec2(0.0f, 0.0f),
                     1.0f,
-                    true);
+                    false);
+            }
+        }
+        for (int i = 1; i < 6; i++) {
+            if (i == 5) {
+                WaterDeeltjes.emplace_back(Vec2(-0.5f * (float)i, squaroot3div2 * (float)i),
+                    Vec2(0.0f, 0.0f),
+                    Vec2(0.0f, 0.0f),
+                    1.0f,
+                    true); // fix outer particles in grid
+
+            }
+            else {
+                WaterDeeltjes.emplace_back(Vec2(-0.5f * (float)i, squaroot3div2 * (float)i),
+                    Vec2(0.0f, 0.0f),
+                    Vec2(0.0f, 0.0f),
+                    1.0f,
+                    false);
+            }
+        }
+        for (int i = 1; i < 5; i++) {
+            if (i == 4) {
+                WaterDeeltjes.emplace_back(Vec2(-0.5f * (float)i, -squaroot3div2 * (float)i),
+                    Vec2(0.0f, 0.0f),
+                    Vec2(0.0f, 0.0f),
+                    1.0f,
+                    true); // fix outer particles in grid
+
+            }
+            else {
+                WaterDeeltjes.emplace_back(Vec2(-0.5f * (float)i, -squaroot3div2 * (float)i),
+                    Vec2(1.0f, 0.0f),
+                    Vec2(0.0f, 0.0f),
+                    1.0f,
+                    false);
             }
         }
         // vul de burenlijst (als afstand kleiner dan cutoff dan buren)
@@ -63,6 +97,8 @@ public:
             }
         }
     }
+private:
+    float squaroot3div2 = 0.5f * std::sqrt(3.0f);
 };
 
 class Simulator {
@@ -101,6 +137,83 @@ private:
             offset = eneDeeltje.offsets[i] / stringTns;
             delta_v = eneDeeltje.velocity - eneDeeltje.neighbours[i]->velocity;
             eneDeeltje.force = eneDeeltje.force - direction * (distance - offset) * springCst
+                - eneDeeltje.velocity * springDmp; // last term is damping
+            eneDeeltje.newAcceleration = eneDeeltje.force / eneDeeltje.mass;
+        }
+    }
+
+    void updatePositions(ParticleSystem* deeltjessysteem, const float timestep) {
+        const float timeStep2Div2 = 0.5f * timeStep * timeStep;
+        for (auto& deeltje : deeltjessysteem->WaterDeeltjes) {
+            if (!deeltje.fixed && deeltje.simulable) { // if particle is not fixed, and simulable
+                deeltje.oldAcceleration = deeltje.newAcceleration; // nieuw wordt oud, nu zijn oud en nieuw gelijk
+                deeltje.position = deeltje.position
+                    + deeltje.velocity * timestep
+                    + deeltje.newAcceleration * timeStep2Div2;
+            }
+        }
+    }
+
+    void updateForcesAndAccelerations(ParticleSystem* deeltjessysteem, const float springCsnt, const float springDmper, const float stringTnsn) {
+        for (auto& deeltje : deeltjessysteem->WaterDeeltjes) {
+            if (!deeltje.fixed && deeltje.simulable) {
+                calculateForcesAndAccelerations(deeltje, springCsnt, springDmper, stringTnsn);
+            }
+        }
+    }
+
+    void updateVelocities(ParticleSystem* deeltjessysteem, const float timestep) {
+        const float timeStepDiv2 = 0.5f * timeStep;
+        for (auto& deeltje : deeltjessysteem->WaterDeeltjes) {
+            if (!deeltje.fixed && deeltje.simulable) {
+                deeltje.velocity = deeltje.velocity
+                    + (deeltje.oldAcceleration + deeltje.newAcceleration) * timeStepDiv2;
+            }
+        }
+    }
+};
+
+class SimulatorFPUT {
+public:
+
+    const float springConstant;
+    const float springDamping;
+    const float stringTension;
+    const float alpha;
+    const float timeStep;
+
+    SimulatorFPUT(float SpringConst, float SpringDamp, float StringTension, float Alpha, float TimeStep) :
+        springConstant(SpringConst),
+        springDamping(SpringDamp),
+        stringTension(StringTension),
+        alpha(Alpha),
+        timeStep(TimeStep)
+    {};
+
+    void verletSolver(ParticleSystem* ps) {
+        updatePositions(ps, timeStep);
+        updateForcesAndAccelerations(ps, springConstant, springDamping, stringTension);
+        updateVelocities(ps, timeStep);
+    }
+
+private:
+
+    float distance = 0.0f;
+    float offset = 0.0f;
+    Vec2 delta_v = Vec2(0.0f, 0.0f);
+    Vec2 direction = Vec2(0.0f, 0.0f);
+
+    void calculateForcesAndAccelerations(waterDeeltje& eneDeeltje, const float springCst, const float springDmp, const float stringTns) {
+        eneDeeltje.force = Vec2(0.0f, 0.0f); // set force of previous timestep to zero
+        for (int i = 0; i < eneDeeltje.neighbours.size(); i++) {
+            distance = eneDeeltje.position.DistanceTo(eneDeeltje.neighbours[i]->position); // REMEMBER the array neighbnours contains pointers!
+            direction = eneDeeltje.position.DirectionTo(eneDeeltje.neighbours[i]->position);
+            offset = eneDeeltje.offsets[i] / stringTns;
+            delta_v = eneDeeltje.velocity - eneDeeltje.neighbours[i]->velocity;
+            eneDeeltje.force = eneDeeltje.force
+                - ((direction * (distance - offset))
+                    - direction * (distance - offset)
+                    * alpha * (distance - offset)) * springCst
                 - direction * springDmp * (delta_v.Dot(direction)); // last term is damping
             eneDeeltje.newAcceleration = eneDeeltje.force / eneDeeltje.mass;
         }
@@ -133,202 +246,6 @@ private:
                 deeltje.velocity = deeltje.velocity
                     + (deeltje.oldAcceleration + deeltje.newAcceleration) * timeStepDiv2;
             }
-        }
-    }
-};
-
-// ALTERNATIVE MET BENDING STRESS
-
-class waterDeeltjeB {
-public:
-    Vec2 position;
-    Vec2 velocity;
-    Vec2 oldAcceleration = Vec2(0.0f, 0.0f);
-    Vec2 newAcceleration; // de verschillende definities van acc hebben met solver te maken
-    float angle = M_PI; // voor angle bending mode, curvature cost
-
-    Vec2 force = Vec2(0.0f, 0.0f);
-    std::vector<waterDeeltjeB*> neighbours = std::vector<waterDeeltjeB*>();
-    std::vector<float> offsets = std::vector<float>();
-
-    float mass;
-    bool simulable; // is the particle motion dictated by forces (true), or static/other (false)?
-
-    waterDeeltjeB(Vec2 initPos, Vec2 initVel, Vec2 initAcc, float massParticle, bool simulatable) :
-        position(initPos), // initializer list
-        velocity(initVel),
-        newAcceleration(initAcc),
-        mass(massParticle),
-        simulable(simulatable)
-    {}
-};
-
-class ParticleSystemB {
-public:
-    std::vector<Vec2> position = std::vector<Vec2>();
-    std::vector<Vec2> velocity = std::vector<Vec2>();
-    std::vector<Vec2> oldAcc = std::vector<Vec2>();
-    std::vector<Vec2> newAcc = std::vector<Vec2>();
-    std::vector<Vec2> force = std::vector<Vec2>();
-    std::vector<float> angle = std::vector<float>();
-    std::vector<float> offset = std::vector<float>();
-    std::vector<uint8_t> simulable;
-    float mass = 1.0f;
-
-    ParticleSystemB(const int N_Particles, const float r_cutoff) {
-
-        for (int i = 0; i < N_Particles; i++) {
-            position.emplace_back(Vec2((float)i, 0.0f)); // EMPLACE BACK
-            newAcc.emplace_back(Vec2(0.0f, 0.0f));
-            oldAcc.emplace_back(Vec2(0.0f, 0.0f));
-            force.emplace_back(Vec2(0.0f, 0.0f));
-            angle.emplace_back(0.0f);
-            if (i == 0 || i == N_Particles - 1) {
-                velocity.emplace_back(Vec2(0.0f, 0.0f));
-                simulable.emplace_back(false);
-            }
-            else {
-                velocity.emplace_back(Vec2(0.0f, 1.0f));
-                simulable.emplace_back(true);
-            }
-        }
-
-        // vul de burenlijst (als afstand kleiner dan cutoff dan buren)
-        for (int i = 0; i < N_Particles; i++) {
-            for (int j = 0; j < N_Particles; j++) {
-                if (i != j && position[i].DistanceTo(position[j]) < r_cutoff) {
-                    offset.emplace_back(
-                        position[i].DistanceTo(position[j])); // fill offsets list
-                }
-            }
-        }
-    }
-};
-
-class SimulatorB {
-public:
-
-    const float springConstant;
-    const float springDamping;
-    const float stringTension;
-    const float stringStress;
-    const float timeStep;
-    const float timeStepDiv2 = 0.5f * timeStep;
-    const float timeStep2Div2 = 0.5f * timeStep * timeStep;
-
-    SimulatorB(float SpringConst, float SpringDamp, float StringTension, float StringStress, float TimeStep) :
-        springConstant(SpringConst),
-        springDamping(SpringDamp),
-        stringTension(StringTension),
-        stringStress(StringStress),
-        timeStep(TimeStep)
-    {};
-
-    void verletSolver(ParticleSystemB* ps) {
-        updatePositions(ps);
-        updateForcesAndAccelerations(ps);
-        updateVelocities(ps);
-    }
-
-private:
-
-    float distance = 0.0f;
-    float a = 0.0f; // distance to left neighbour
-    float b = 0.0f; // distance to right neighbour
-    float c = 0.0f; // distance between neighbours
-    float offsetPart = 0.0f;
-    Vec2 delta_v = Vec2(0.0f, 0.0f);
-    Vec2 direction = Vec2(0.0f, 0.0f);
-    std::vector<Vec2> directions = { Vec2{0.0f, 0.0f}, Vec2{0.0f, 0.0f} };
-    std::vector<Vec2> angleForce = { Vec2{0.0f, 0.0f}, Vec2{0.0f, 0.0f}, Vec2{0.0f, 0.0f} };
-
-    void calculateForcesAndAccelerations(std::vector<Vec2>& position, 
-                                        std::vector<Vec2>& velocity, 
-                                        std::vector<Vec2>& force, 
-                                        std::vector<Vec2>& accelerationNew,
-                                        std::vector<float>& angle,
-                                        const std::vector<float>& offsets,
-                                        const float mass) {
-        
-    }
-
-    void updatePositions(ParticleSystemB* deeltjessysteem) {
-        for (int i = 0; i < deeltjessysteem->position.size(); ++i) {
-            if (deeltjessysteem->simulable[i]) {
-                deeltjessysteem->oldAcc[i] = deeltjessysteem->newAcc[i]; // nieuw wordt oud, nu zijn oud en nieuw gelijk
-                deeltjessysteem->position[i] = deeltjessysteem->position[i]
-                    + deeltjessysteem->velocity[i] * timeStep
-                    + deeltjessysteem->newAcc[i] * timeStep2Div2;
-            }
-        }
-    }
-
-    void updateForcesAndAccelerations(ParticleSystemB* ds) {
-        // skip the first and last particle, since we need triplets, and outer particles have only one neighbour
-        for (int i = 1; i < ds->position.size() - 1; ++i) {
-            // calculate the distances
-            a = ds->position[i].DistanceTo(ds->position[i - 1]);
-            b = ds->position[i].DistanceTo(ds->position[i + 1]);
-
-            // then we determine normalized directional vectors
-            directions[0] = ds->position[i].DirectionTo(ds->position[i - 1]);
-            directions[1] = ds->position[i].DirectionTo(ds->position[i + 1]);
-
-            offsetPart = ds->offset[i] / stringTension; // slimme manier he, om tension te definieren
-
-            // calculate the SPRING FORCE for both neighbours:
-            // left neighbour:
-            delta_v = ds->velocity[i] - ds->velocity[i - 1];
-            ds->force[i] = ds->force[i] - directions[0] * (a - offsetPart) * springConstant
-                - directions[0] * springDamping * (delta_v.Dot(directions[0]));
-
-            // right neighbour:
-            delta_v = ds->velocity[i] - ds->velocity[i + 1];
-            ds->force[i] = ds->force[i] - directions[1] * (b - offsetPart) * springConstant
-                - directions[1] * springDamping * (delta_v.Dot(directions[1]));
-
-            // niet vergeten deze weer weg te doen:
-            ds->newAcc[i] = ds->force[i] / ds->mass;
-            ds->force[i] = Vec2{ 0.0f, 0.0f }; // reset forces
-
-            // vervolg, nu ANGLE BENDING
-            
-            /*
-            c = ds->position[i - 1].DistanceTo(ds->position[i + 1]); // neighbour-to-neighbour distance (for calculation of theta)
-
-            // from distances we calculate the angle between 3 particles via inverse cosine rule
-            ds->angle[i] = std::acos((a * a + b * b - c * c) / (2 * a * b));
-
-            // calculate angle bending forces via distances, directions and angle
-            angleForce[0] = Vec2(directions[0].y, -directions[0].x) * (ds->angle[i] - M_PI) * (-stringStress / a); // left particle
-            angleForce[2] = Vec2(-directions[1].y, directions[1].x) * (ds->angle[i] - M_PI) * (-stringStress / b); // right particle
-            angleForce[1] = (angleForce[0] + angleForce[2]) * (-1.0f); // middle particle is the opposite of the other two forces
-
-            // add the angle forces to the total force:
-            ds->force[i - 1] = ds->force[i - 1] + angleForce[0];
-            ds->force[i] = ds->force[i] + angleForce[1];
-            ds->force[i + 1] = ds->force[i + 1] + angleForce[2];
-
-            // only update the acceleration of the left most particle in the trio, since it has seen all the forces:
-
-            ds->newAcc[i - 1] = ds->force[i - 1] / ds->mass;
-            ds->force[i - 1] = Vec2{ 0.0f, 0.0f }; // reset forces
-
-            if (i == ds->position.size() - 2) { // if you reached the last particle, update acc of middle particle (i) too
-                ds->newAcc[i] = ds->force[i] / ds->mass;
-                ds->force[i] = Vec2{ 0.0f, 0.0f }; // reset forces
-            }
-            */
-        }
-        
-    }
-
-    void updateVelocities(ParticleSystemB* deeltjessysteem) {
-        for (int i = 0; i < deeltjessysteem->position.size(); ++i) {
-            if (deeltjessysteem->simulable[i]) {
-                deeltjessysteem->velocity[i] = deeltjessysteem->velocity[i]
-                    + (deeltjessysteem->oldAcc[i] + deeltjessysteem->newAcc[i]) * timeStepDiv2;
-            } 
         }
     }
 };
